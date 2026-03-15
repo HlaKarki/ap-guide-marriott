0c740774a1e50ea78c7e780b14fde6e24d44139a
hlakarki <kshap.karki@gmail.com>
2026-03-15 16:56:13 -0400

    feat: linter setup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router';
+import React from "react";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({ component: App });
+export const Route = createFileRoute("/")({ component: App });
 
 function App() {
   return (
-    <div className="min-h-screen bg-linear-to-b from-slate-900 via-slate-800 to-slate-900">
+    <div className="min-h-screen">
       <section className="py-16 px-6 max-w-7xl mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          HI!
-        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">HI!</div>
       </section>
     </div>
   );
