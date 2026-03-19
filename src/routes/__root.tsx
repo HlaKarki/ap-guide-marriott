a8d5acd8b8fd3b1c14a8a6cf96f2450a27665319
hlakarki <kshap.karki@gmail.com>
2026-03-19 02:30:43 -0400

    feat: favicons, manifest

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,13 +28,39 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "TanStack Start Starter",
+        title: "AP Guide - Marriott",
       },
     ],
     links: [
       {
         rel: "stylesheet",
         href: appCss,
+      },
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon/favicon.ico",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon/favicon-16x16.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/favicon/apple-touch-icon.png",
+      },
+      {
+        rel: "manifest",
+        href: "/favicon/site.webmanifest",
       },
     ],
   }),
